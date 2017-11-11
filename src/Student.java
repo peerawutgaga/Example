ba26b9bb2d3da7f1c525e7e51a3bc26da5c17e83
Peerawutgaga <pee.luang45@gmail.com>
2017-11-11 09:31:56 +0700

    Add Student Class

---
+++ src/Student.java
@@ -0,0 +1,10 @@
+
+public class Student {
+	private String studentID;
+	private String password;
+	public Student(String studentID, String password)
+	{
+		this.studentID = studentID;
+		this.password = password;
+	}
+}
