8864266387ed49dab8b5d04b5e4669a96145ee92
Peerawut Luengruengroj <peerawut_lueng@hotmail.com>
2017-11-16 09:33:33 +0700

    Delete driver

--- src/Driver/StudentDriver.java
+++ src/Driver/StudentDriver.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import RegisterSystem.Student;
 
-class StudentDriver {
+class MaiDriver {
 
 	@Test
 	void testGetGPAX() {
