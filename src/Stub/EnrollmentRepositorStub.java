a474da0a0bfb08c020e2a3a55ed2e75c4b35bfaf
Peerawutgaga <pee.luang45@gmail.com>
2017-11-17 11:55:10 +0700

    Make stub show parameter value

--- src/Stub/EnrollmentRepositorStub.java
+++ src/Stub/EnrollmentRepositorStub.java
@@ -4,9 +4,10 @@ import java.util.List;
 
 import RegisterSystem.Enrollment;
 
-public class EnrollmentRepository {
+public class EnrollmentRepositorStub {
 	public static List<Enrollment> searchByStudentID(String studentID)
 	{
+		System.out.println(studentID);
 		return null;
 	}
 }
--- src/Stub/GPAXCalculatorStub.java
+++ src/Stub/GPAXCalculatorStub.java
@@ -3,11 +3,8 @@ package Stub;
 public class GPAXCalculatorStub {
 	public static double calGPAX(String studentID)
 	{
+		System.out.println(studentID);
 		return 0;
 	}
 	
-	public static int getGradeNumber(char gradeLetter)
-	{
-		return 0;
-	}
 }
