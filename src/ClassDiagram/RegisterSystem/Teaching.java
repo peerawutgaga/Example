46be23a0c13767dd96edec836fa28ccaa1d88732
peerawutgaga <pee.luang45@gmail.com>
2018-04-16 08:19:30 +0700

    Change file package.

---
+++ src/ClassDiagram/RegisterSystem/Teaching.java
@@ -0,0 +1,41 @@
+package ClassDiagram.RegisterSystem;
+
+public class Teaching {
+	private String teacherID;
+	private String courseID;
+	private int section;
+	private String dateAndTime;
+	private int semester;
+	private String year;
+	public Teaching(String teacherID, String courseID, int section)
+	{
+		this.teacherID = teacherID;
+		this.courseID = courseID;
+		this.section = section;
+	}
+	public int getSection() {
+		return section;
+	}
+	public void setSection(int section) {
+		this.section = section;
+	}
+	public String getDateAndTime() {
+		return dateAndTime;
+	}
+	public void setDateAndTime(String dateAndTime) {
+		this.dateAndTime = dateAndTime;
+	}
+	public String getTeacherID() {
+		return teacherID;
+	}
+	public String getCourseID() {
+		return courseID;
+	}
+	public int getSemester() {
+		return semester;
+	}
+	public String getYear() {
+		return year;
+	}
+	
+}
