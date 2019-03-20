3fd411ceb7c282cda68fbca8d77760c54d2e8e15
peerawutgaga <pee.luang45@gmail.com>
2019-03-20 21:17:34 +0700

    Fix driver return value assertion

--- src/driver/enrollment/GraderDriver.java
+++ src/driver/enrollment/GraderDriver.java
@@ -8,6 +8,6 @@ public class GraderDriver {
     @Test
     public void testGetGPAX(){
         double returnValue = Grader.getGPAX("1233264");//Random Value
-        Assert.assertEquals(165.215645,54645.146546,132465.6646);//Random Value
+        Assert.assertEquals(165.215645,returnValue,132465.6646);//Random Value
     }
 }
